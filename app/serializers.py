f417e1047625c000b1a848a0db5aa8112346bbd4
gabriel <maccacherog@gmail.com>
2026-05-22 08:42:43 -0300

    autenticação teste

--- app/serializers.py
+++ app/serializers.py
@@ -1,6 +1,7 @@
 # app/serializers.py
 from rest_framework import serializers
 from app.models import Usuario, Empresa, Instituicao, Contrato, SistemaValidador
+from django.contrib.auth import authenticate
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,3 +32,42 @@ class SistemaValidadorSerializer(serializers.ModelSerializer):
         model = SistemaValidador
         fields = ['id', 'contrato']
         read_only_fields = ['id']
+
+
+# --- Autenticação ---
+# TESTAR AUTENTICAÇÃO
+#confirmar com o jonh
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = Usuario
+        fields = ['id', 'cpf', 'username', 'email', 'password']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        usuario = Usuario(**validated_data)
+        usuario.set_password(password)
+        usuario.save()
+        return usuario
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError('Credenciais inválidas.')
+        return user
+
+class SistemaValidadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SistemaValidador
+        fields = ['id', 'contrato']
+        read_only_fields = ['id']
+
+
